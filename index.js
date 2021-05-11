07ef555c412880395158ee0649742df49a5bbef1
kevinkace <kevinacameron@hotmail.com>
2021-05-11 16:53:14 -0700

    update deps and ver

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 "use strict";
 
-var through = require("through2"),
-    tab     = /\t/g;
+const through = require("through2");
+
+const tab = /\t/g;
 
 module.exports = function(file, options) {
-    var opt  = options || false,
-        size = opt.spaces || 4,
-        str  = new Array(size + 1).join(" ");
-    
+    const opt  = options || false;
+    const size = opt.spaces || 4;
+    const str  = new Array(size + 1).join(" ");
+
     return through(function write(chunk, enc, next) {
         next(null, chunk.toString().replace(tab, str));
     });
--- test/test.js
+++ test/test.js
@@ -1,71 +1,71 @@
 "use strict";
 
-var test       = require("tape"),
-    concat     = require("concat-stream"),
-    from       = require("from2-string"),
-    browserify = require("browserify"),
-    
-    detabbify = require("../");
+const test       = require("tape");
+const concat     = require("concat-stream");
+const from       = require("from2-string");
+const browserify = require("browserify");
+
+const detabbify = require("../");
 
 test("exports", function(t) {
     t.plan(1);
-    
+
     t.equal(typeof detabbify, "function", "Should be a function");
 });
 
 test("defaults", function(t) {
-    var str;
-    
+    let str;
+
     t.plan(1);
-    
+
     str = detabbify("./fake.js");
-    
+
     str.pipe(concat(function(out) {
         t.equal(out.toString(), "    ", "Should replace \t with 4 spaces");
     }));
-    
+
     str.write("\t");
     str.end();
 });
 
 test("options", function(t) {
-    var str;
-    
+    let str;
+
     t.plan(1);
-    
+
     str = detabbify("./fake.js", { spaces : 2 });
-    
+
     str.pipe(concat(function(out) {
         t.equal(out.toString(), "  ", "Should replace \t with 2 spaces");
     }));
-    
+
     str.write("\t");
     str.end();
 });
 
 test("browserify", function(t) {
-    var build = browserify({
+    let build = browserify({
             entries : from("\t'hello';")
         });
-    
+
     t.plan(1);
-    
+
     build.transform(detabbify);
-    
+
     build.bundle(function(err, out) {
         t.ok(out.toString().indexOf("    'hello';") > -1, "Replaced file contents correctly");
     });
 });
 
 test("browserify + options", function(t) {
-    var build = browserify({
+    let build = browserify({
             entries : from("\t'hello';")
         });
-    
+
     t.plan(1);
-    
+
     build.transform(detabbify, { spaces : 2 });
-    
+
     build.bundle(function(err, out) {
         t.ok(out.toString().indexOf("  'hello';") > -1, "Replaced file contents correctly");
     });
